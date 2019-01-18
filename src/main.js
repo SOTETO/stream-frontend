384172454bfbc8118a5da243a317226ec5a0316a
Johann Sell <sell@informatik.hu-berlin.de>
2019-01-18 21:34:19 +0100

    init vca frontend app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { WidgetTopNavigation, WidgetBottomNavigation } from 'vca-widget-navigation'
 import router from './router'
+import VueI18n from 'vue-i18n'
+import en from '@/lang/en_US.json';
+import de from '@/lang/de_DE.json';
+
+Vue.use(VueI18n);
+
+const locale =  navigator.language;
+const i18n = new VueI18n({
+    locale: locale,
+    messages: {
+        'en-US': en,
+        'de-DE': de,
+        'de': de,
+        'en': en
+    }
+});
+
+Vue.use(WidgetBottomNavigation, { 'i18n': i18n })
+Vue.use(WidgetTopNavigation, { 'i18n': i18n })
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
