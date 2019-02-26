34fbd4819c95b5d145b631f0043518305865d3bf
Johann Sell <sell@informatik.hu-berlin.de>
2019-02-26 16:29:59 +0100

    add auth handshake and handling

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import store from './store'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
     {
       path: '/',
@@ -22,17 +23,80 @@ export default new Router({
     {
         path: '/donations',
         name: 'donations',
-        component: () => import('./views/Donations.vue')
+        component: () => import('./views/Donations.vue'),
+        meta: {
+          'roles': ['Admin', 'Employee', { 'name': 'VolunteerManager' }] //'Admin',
+        }
     },
     {
         path: '/donations/add',
         name: 'donations-add',
-        component: () => import('./views/DonationsAdd.vue')
+        component: () => import('./views/DonationsAdd.vue'),
+        meta: {
+          'roles': ['Admin', 'Employee', { 'name': 'VolunteerManager' }] //'Admin',
+        }
     },
     {
       path: '/household',
       name: 'household',
-      component: () => import('./views/Household.vue')
+      component: () => import('./views/Household.vue'),
+      meta: {
+        'roles': ['Admin', 'Employee', { 'name': 'VolunteerManager' }] //'Admin',
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.hasOwnProperty("roles"))) {
+        function checker () {
+            // Now, check the roles for the given entry!
+            if(!store.getters['user/isError']) {
+                var records = to.matched.filter(record => record.meta.hasOwnProperty("roles"))
+                var fulfillsRole = records.reduce((fulfills, record) => {
+                    return fulfills || store.getters['user/is'](record.meta.roles)
+                }, records.length === 0)
+
+                if(fulfillsRole) {
+                    next();
+                } else {
+                    window.location.replace('/arise/#/error/403')
+                }
+            } else {
+                switch (store.getters['user/getErrorCode']) {
+                    case 401:
+                        // Not Authenticated!
+                        window.location.replace('/arise/#/signin?redirect=' + to.fullPath)
+                        break;
+                    case 403:
+                        // Forbidden!
+                        window.location.replace('/arise/#/error/403')
+                        break;
+                    case 404:
+                        // redirect 404 error page
+                        window.location.replace('/arise/#/error/404')
+                        break;
+                    case 500:
+                        // redirect 500 error page
+                        window.location.replace('/arise/#/error/500')
+                        break;
+                }
+            }
+        }
+        // only for init - checks if there is already a user, initiates OAuth handshake otherwise
+        var u = store.getters['user/get']
+        if(u === null) {
+            // Handshake!
+            store.dispatch('user/init').then(() => {
+                checker()
+            })
+        } else {
+            checker()
+        }
+
+    } else {
+        next();
+    }
+});
+
+export default router
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import donations from './modules/donations'
 import household from './modules/household'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
@@ -10,7 +11,8 @@ const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
     modules: {
         donations,
-        household
+        household,
+        user
     },
     strict: debug
 })
