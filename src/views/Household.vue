9f5d13a81d788c17b1e041f4890a66156139140b
Johann Sell <sell@informatik.hu-berlin.de>
2019-03-27 12:17:13 +0100

    better styling and alignment of expenses in household tabel

--- src/views/Household.vue
+++ src/views/Household.vue
@@ -2,58 +2,7 @@
     <VcAFrame>
         <VcAColumn size="70%">
             <VcABox :first="true" :title="$t('household.header.box.list')">
-                <table class="exposes">
-                    <thead>
-                        <tr>
-                            <th>{{ $t("household.header.table.what") }}</th>
-                            <th>{{ $t("household.header.table.wherefor") }}</th>
-                            <th>{{ $t("household.header.table.crew") }}</th>
-                            <th>{{ $t("household.header.table.amount") }}</th>
-                            <th>{{ $t("household.header.table.date") }}</th>
-                            <th>{{ $t("household.header.table.supporter") }}</th>
-                            <th>{{ $t("household.header.table.state") }}</th>
-                            <th>{{ $t("household.header.table.process") }}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="expose in exposes" :key="expose.id">
-                            <td>
-                                <span>{{ expose.what }}</span><br />
-                                <button
-                                        class="vca-button-inline"
-                                        @click="editState(expose)"
-                                >{{ $t("household.buttons.edit") }}</button>
-                            </td>
-                            <td>{{ expose.wherefor }}</td>
-                            <td><Tag :crew="true" :uuid="expose.author" /></td>
-                            <td  class="vca-amount-wrapper">
-                                <span class="vca-amount">
-                                    {{ expose.amount[expose.amount.length - 1] }}
-                                </span>
-                                <div class="vca-amount-previous">
-                                    <span v-if="expose.amount.length > 1" class="vca-amount">
-                                        {{ expose.amount[expose.amount.length - 2] }}
-                                    </span>
-                                    <span v-if="expose.amount.length > 2" class="vca-amount">...</span>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="dates">
-                                    <span>{{ $t("household.hints.dates.created", { "date":  $d(new Date(expose.date.created), "short") }) }}</span>
-                                    <span v-if="expose.date.created !== expose.date.updated">
-                                        {{ $t("household.hints.dates.updated", { "date":  $d(new Date(expose.date.created), "short") }) }}
-                                    </span>
-                                </div>
-                            </td>
-                            <td><Tag v-for="uuid in expose.supporter" :key="uuid" :uuid="uuid" /></td>
-                            <td>{{ $t("household.states." + expose.state) }}</td>
-                            <td>
-                                <span>{{ $t("household.process.VolunteerManager." + expose.processing.VolunteerManager) }}</span><br />
-                                <span>{{ $t("household.process.Employee." + expose.processing.Employee) }}</span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <ExpenseList @vca-edit-expose="editState" />
             </VcABox>
         </VcAColumn>
         <VcAColumn size="20%">
@@ -79,17 +28,17 @@
 </template>
 
 <script>
-    import { mapActions, mapGetters } from 'vuex'
+    import { mapGetters } from 'vuex'
     import { VcAFrame, VcAColumn, VcABox } from 'vca-widget-base'
     import 'vca-widget-base/dist/vca-widget-base.css'
-    import { Tag } from 'vca-widget-user'
     import ExpenseForm from '../components/household/ExpenseForm'
     import ExpenseStateTransition from '../components/household/ExpenseStateTransition'
+    import ExpenseList from '../components/household/ExpenseList'
 
     export default {
         name: "Household",
         components: {
-            VcAFrame, VcAColumn, VcABox, ExpenseForm, ExpenseStateTransition, Tag
+            VcAFrame, VcAColumn, VcABox, ExpenseForm, ExpenseStateTransition, ExpenseList
         },
         data () {
             var editableDefault = {
@@ -101,12 +50,8 @@
                 "editable": JSON.parse(JSON.stringify(editableDefault))
             }
         },
-        created () {
-            this.init()
-        },
         computed: {
             ...mapGetters('household', {
-                exposes: 'overview',
                 byId: 'byId'
             }),
             isEditState () {
@@ -114,9 +59,6 @@
             }
         },
         methods: {
-            ...mapActions('household', [
-                'init', // map `this.init()` to `this.$store.dispatch('donations/init')`
-            ]),
             editState (expose) {
                 this.editable.value = this.byId(expose.id)
                 this.editable.key = expose.id
@@ -129,19 +71,4 @@
 </script>
 
 <style scoped lang="less">
-    .exposes {
-        width: 100%;
-    }
-    .dates {
-        display: flex;
-        flex-direction: column;
-    }
-    .vca-amount-wrapper {
-        .vca-amount:not(:first-child) {
-            margin-left: 0.2em
-        }
-        .vca-amount-previous {
-            color: #8a8a8a;
-        }
-    }
 </style>
