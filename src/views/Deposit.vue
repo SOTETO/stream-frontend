faeb2407a8207a7b727f971876b9d48d0c978e24
Johann Sell <sell@informatik.hu-berlin.de>
2019-06-24 21:03:32 +0200

    add pagination and sort

--- src/views/Deposit.vue
+++ src/views/Deposit.vue
@@ -2,7 +2,14 @@
   <VcAFrame>
     <VcAColumn size="90%">
       <VcABox :first="true"  :title="$t('deposit.header.box')">
+        <ListMenu :fields="sortFields" store="deposits" />
+        <button v-if="hasPrevious" v-on:click="pageDown()" class="paginate">
+          {{ $tc('pagination.previous', pageGet.previous, { 'number': pageGet.previous }) }}
+        </button>
         <DepositList/>
+        <button v-if="hasNext" v-on:click="pageUp()" class="paginate">
+          {{ $tc('pagination.next', pageGet.next, { 'number': pageGet.next }) }}
+        </button>
       </VcABox>
     </VcAColumn>
   </VcAFrame>
@@ -11,10 +18,71 @@
 <script>
   import { VcAFrame, VcAColumn, VcABox } from 'vca-widget-base'
   import DepositList from '@/components/deposit/DepositList'
+  import ListMenu from '../components/utils/ListMenu'
+  import { mapGetters, mapActions } from 'vuex'
+
   export default {
     name: "deposits",
     components: {
-      VcAFrame, VcAColumn, VcABox, DepositList
+      VcAFrame, VcAColumn, VcABox, DepositList, ListMenu
+    },
+    computed: {
+        ...mapGetters('donations', {
+            pageGet: 'page'
+        }),
+        hasPrevious () {
+            return this.pageGet.previous > 0
+        },
+        hasNext () {
+            return this.pageGet.next > 0
+        },
+        sortFields() {
+            return [
+                {
+                    "value": "deposit.created",
+                    "label": this.$t("deposit.table.head.created")
+                },
+                {
+                    "value": "deposit.date_of_deposit",
+                    "label": this.$t("deposit.table.head.date")
+                },
+                {
+                    "value": "deposit.full_amount",
+                    "label": this.$t("deposit.table.head.amount")
+                },
+                {
+                    "value": "deposit.confirmed",
+                    "label": this.$t("deposit.table.head.state")
+                },
+                {
+                    "value": "deposit.crew",
+                    "label": this.$t("deposit.table.head.crew")
+                }
+            ]
+        }
+    },
+    methods: {
+        ...mapActions('donations', [
+            'page'
+        ]),
+        pageDown () {
+            this.page(true)
+        },
+        pageUp () {
+            this.page(false)
+        }
     }
   }
 </script>
+
+<style scoped lang="less">
+  @import '../assets/less/general.less';
+
+  .paginate {
+    width: 100%;
+    height: 2.5em;
+    .inputElement();
+    cursor: pointer;
+    background: none;
+  }
+</style>
